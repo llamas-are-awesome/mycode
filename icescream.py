10c14af0294dd5fe2296b9c4d63d03387e75b129
llamas-are-awesome <llamas-are-awesome@users.noreply.github.com>
2022-02-22 17:33:40 +0000

    Icecream project

---
+++ icescream.py
@@ -0,0 +1,13 @@
+icecream = ["indentation", "spaces"]
+
+tlgstudents= ["Bryan", "Colin", "Erik", "Gregory", "John", "Kishor", "Leia", "Maria", "Monte", "Jarrad", "Pemba", "Don", "Tim", "Travis", "Trung"]
+
+icecream.append(4)
+
+studentid = int(input("Enter a number from 0 - 14: "))
+
+selection = tlgstudents[studentid]
+
+print(selection , " always uses " , icecream[2]  ," " ,  icecream[1])
+
+
